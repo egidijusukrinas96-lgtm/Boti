18d365c0dc79f4561b43052c57f7d57493159144
egidijusukrinas96-lgtm <egidijusukrinas96@gmail.com>
2026-09-06 14:09:59 +0300

    Add Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.adrenaline.wallet',
+  appName: 'Adrenaline Wallet',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
